405212940a1afeb5571a46c25947a41a2a680e02
trinitybest <hutianyou6@gmail.com>
2016-05-11 15:56:06 +1200

    Update 6. ZigZag Conversion.js

--- 6. ZigZag Conversion.js
+++ 6. ZigZag Conversion.js
@@ -5,25 +5,26 @@
  */
 var convert = function(s, numRows) {
     var originalStr = s;
+    //special issues
     if(numRows === 1 || originalStr.length <= numRows)
         return originalStr;
+    //create a two dimension array
     var zigzag = new Array(numRows);
     for (var j=0; j<numRows; j++){
     zigzag[j]= new Array();
     }
-    console.log(zigzag);
+    //create row and step
     var row = 0, step = 1;
     for(var i = 0; i<originalStr.length; i++){
     		
         zigzag[row]+=originalStr[i];
-        //console.log("i", originalStr[i], "row", row)
-        //console.log("zigzag",zigzag[row])
+        //when row equals 0, we move forward 1 step per time
         if(row === 0)
             step = 1;
+        //when row equals numRows-1, we move backwards 1 step per time
         else if(row === numRows-1)
             step = -1;
         row += step;
     }
-    return zigzag.join("");
+    return zigzag.join("");//join strings together
 };
-
